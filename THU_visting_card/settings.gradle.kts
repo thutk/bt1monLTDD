66548bcae4105d2d3850e0eb70e01579972ab2d7
thutk <nguyenxuanthu10092003@gmail.com>
2024-01-27 22:31:05 +0700

    add file

---
+++ THU_visting_card/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "THU_visting_card"
+include(":app")
